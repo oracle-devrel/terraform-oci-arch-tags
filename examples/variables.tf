7b727650458afb65b740bef4b916f87ba8b398b8
Phil Wilkins <phil@mp3monster.org>
2022-07-15 16:06:59 +0100

    random variable no longer required

--- examples/variables.tf
+++ examples/variables.tf
@@ -1,4 +1,4 @@
-## Copyright (c) 2022 Oracle and/or its affiliates.
+## Copyright (c) 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 variable "tenancy_ocid" {}
@@ -7,9 +7,3 @@ variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
-
-/* variable "random_value" {
-  type        = string
-  default     = random_id.tag.hex
-  description = "This is a random value that can be incorporated into the tag to avoid any tag clashing"
-} */
