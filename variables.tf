ae3f847d1ed506cea6bdacfccd071b0f0289baa4
Phil Wilkins <phil@mp3monster.org>
2022-07-15 16:17:18 +0100

    no need for these to be local variable

--- variables.tf
+++ variables.tf
@@ -1,12 +1,5 @@
 ## Copyright (c) 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-variable "compartment_ocid" {}
-
 
 locals { implementation_module = { "implementation_module" = "tags" } }
