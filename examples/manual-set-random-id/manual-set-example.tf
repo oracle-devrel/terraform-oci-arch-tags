aa69513e73df05d79df82d99a55350ea03428c11
Phil Wilkins <phil@mp3monster.org>
2022-07-18 10:41:24 +0100

    Examples separated into their own folders now

--- examples/manual-set-random-id/manual-set-example.tf
+++ examples/manual-set-random-id/manual-set-example.tf
@@ -1,17 +1,18 @@
 ## Copyright (c) 2021, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-module "tags" {
-  #source           = "./.."
+module "tags_manual" {
+  #source = "./../.."
   source           = "github.com/oracle-devrel/terraform-oci-arch-tags"
-  tag_namespace    = "terraform-oci-arch-test-name"
+  tag_namespace    = "terraform-oci-arch-test-name-manual"
   compartment_ocid = var.compartment_ocid
-  release          = "1.1" # this is optional, if not provided then 1.0 is defaulted
+  release          = "1.5" # this is optional, if not provided then 1.0 is defaulted
+  random_id        = "PW25"
 }
 
-resource "oci_logging_log_group" "test_log_group" {
-  display_name   = "OCI Test Log Group for Tagging"
+resource "oci_logging_log_group" "test_log_group-from-manual" {
+  display_name   = "OCILoggingGroupWithMANUALId"
   description    = "This log group exists only to demonstrate the attachment of a predefined tag"
   compartment_id = var.compartment_ocid
-  defined_tags    = module.tags.predefined_tags
+  defined_tags   = module.tags_manual.predefined_tags
 }
